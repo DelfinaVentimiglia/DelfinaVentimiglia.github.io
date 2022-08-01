e826319d90ca26d56b5c8b4de1d0f6f1db8ce41f
Delfina Ventimiglia <69704546+DelfinaVentimiglia@users.noreply.github.com>
2022-08-01 02:44:30 -0300

    Proyecto Final
    Ultimo commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let mainNav = document.getElementById("navbar");
+let navBarToggle = document.getElementById ("hb-btn");
+
+navBarToggle.addEventListener("click", function(){
+
+    mainNav.classList.toggle("active");
+});
+
+let mainFooter = document.getElementById(<button type="button"></button>);
+let buttonBarToggle = document.getElementById("btn btn-dark");
+
+buttonBarToggle.addEventListener("click", function(){
+
+    mainButton.classList.toggle("active");
+})
+
